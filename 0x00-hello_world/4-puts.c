bed64c531e40e83696568b11e546dd160552b18b
yusunad <ysunday5@gmail.com>
2023-09-07 08:13:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Entry Point "A program that prints a line with puts function 
+ * main - Entry Point "A program that prints a line with puts function 
  * Return: 0 (Success)
  */
 int main(void) 
