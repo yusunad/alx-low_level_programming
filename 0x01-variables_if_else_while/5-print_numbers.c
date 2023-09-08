e4217f8a98a1ee06bb84cd65f46ab4abc2a59721
yusunad <ysunday5@gmail.com>
2023-09-08 09:01:42 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+#include <stdio.h>
 
-int main(void) {
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
 
-	int i;
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
-	for (i=0; i<=10; i++)
-
-	printf ("%d", i); 
-
-	printf ("\n")
+	printf("\n");
 
 	return (0);
 }
