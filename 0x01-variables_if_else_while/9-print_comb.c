f24e8ddcf026426d0f6e0c5f77bedc7726564014
yusunad <ysunday5@gmail.com>
2023-09-09 03:57:14 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
-	{
+		for (num = 0; num <= 9; num++)
+		{
 			putchar((num % 10) + '0');
-			if (num == 9)
+				if (num == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
