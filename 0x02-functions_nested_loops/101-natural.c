f0f19c3b9ba6eccf822ce987137b5d51ea8b0ed2
yusunad <ysunday5@gmail.com>
2023-09-12 16:09:15 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-/**
+/*
  * File: 101-natural.c
  * Auth: sunny
  */
@@ -9,7 +9,7 @@
  * main - Lists all the natural numbers below 1024 (excluded)
  *        that are multiples of 3 or 5.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
